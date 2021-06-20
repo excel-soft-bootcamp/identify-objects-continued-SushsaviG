81e803fbf242a5c5c954104e3809def801561da3
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-20 21:42:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student_info _instance = new Student_info();
+            _instance.SetId("100");
+            _instance.SetName("SUSHMA");
+            string idvalue = _instance.GetId();
+            System.Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student info.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Student_info
+    {
+        private string id;
+        private string name;
+        private string marks;
+        public string GetId() { return this.id; }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public string GetMarks()
+        {
+            return this.marks;
+        }
+        public void SetId(string value) { this.id = value; }
+        public void SetName(string value) { this.name = value; }
+        
+    }
+}
